d7eb085860ebd69d86c253c7d8adb7023dd4280d
Morgan Cooper <cooperme5@vcu.edu>
2025-01-29 12:20:39 -0500

    change theme

--- 01_assign1script.R
+++ 01_assign1script.R
@@ -10,9 +10,11 @@ library(ggplot2)
 #plotting mtcars miles per gallon v. number of cylinders
 
 ggplot(data = mtcars, aes(x= hp, y= mpg))+
+  theme_linedraw()+
   geom_point()+
   labs(title = "Miles per Gallon vs. Horsepower",
        y= "Miles per Gallon (mpg)",
        x= "Horsepower (hp)")+
   theme(plot.title = element_text(hjust = 0.5))+
   stat_smooth(method=lm)
+ 
